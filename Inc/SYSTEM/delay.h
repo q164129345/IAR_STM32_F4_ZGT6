3c5a6fc1de1254cd777921ffd6648529d187395b
q164129345 <164129345@qq.com>
2020-05-15 17:35:56 +0800

    first update

---
+++ Inc/SYSTEM/delay.h
@@ -0,0 +1,11 @@
+#ifndef _DELAY_H
+#define _DELAY_H
+
+#include "stm32f4xx.h"
+#include "stm32f4xx_hal.h"
+
+void delay_init(uint8_t SYSCLK);
+void delay_ms(uint16_t nms);
+void delay_us(uint32_t nus);
+#endif
+
